982befde8ed5a30b8a3237f8a503952fa04f0ae1
Roberto AMD <haschell@gmail.com>
2020-01-13 21:29:21 +0100

    Subscripciones y Observadores

--- src/create.js
+++ src/create.js
@@ -1,12 +1,30 @@
 import { displayLog } from './utils';
-import { of, interval, zip } from 'rxjs';
+import { of, interval, zip, Observable } from 'rxjs';
 
 export default () => {
     /** start coding */
 
-    const src1 = interval(300);
-    const src2 = "Hello World!";
-    zip(src1, src2).subscribe(x =>displayLog(x[1]));
+    /*     const src1 = interval(300);
+        const src2 = "Hello World!";
+        zip(src1, src2).subscribe(x => displayLog(x[1])); */
+
+    const hello = Observable.create(function(observer) {
+        observer.next("Hello");
+        setTimeout(() => {
+            observer.next("world");
+            observer.complete();
+        }, 2000);
+    });
+
+    const observer = {
+        next: evt => displayLog(evt),
+        error: err => console.err("[ERR - ", err),
+        complete: () => displayLog("[DONE]")
+    }
+
+    const subscribe = hello.subscribe(observer);
+    const subscribe2 = hello.subscribe(observer);
+    subscribe.unsubscribe();
 
     /** end coding */
 }
